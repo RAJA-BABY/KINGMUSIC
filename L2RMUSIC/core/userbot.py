2977564e720fbc970d4c485b3ef0649a5cc25d37
ʀᴀᴊᴀ ʙᴀʙᴜ <anilkumarmishraji80@gmail.com>
2025-01-03 10:19:41 +0530

    Update userbot.py

--- L2RMUSIC/core/userbot.py
+++ L2RMUSIC/core/userbot.py
@@ -54,8 +54,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("BWF_MUSIC1")
-                await self.one.join_chat("MUSICBOT_OWNER")
+                await self.one.join_chat("TGSTYLENAME")
+                await self.one.join_chat("BOTXPRO1")
             except:
                 pass
             assistants.append(1)
@@ -83,8 +83,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("BWF_MUSIC1")
-                await self.two.join_chat("MUSICBOT_OWNER")
+                await self.two.join_chat("TGSTYLENAME")
+                await self.two.join_chat("BOTXPRO1")
             except:
                 pass
             assistants.append(2)
@@ -112,8 +112,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("BWF_MUSIC1")
-                await self.three.join_chat("MUSICBOT_OWNER")
+                await self.three.join_chat("TGSTYLENAME")
+                await self.three.join_chat("BOTXPRO1")
             except:
                 pass
             assistants.append(3)
@@ -141,8 +141,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("BWF_MUSIC1")
-                await self.four.join_chat("MUSICBOT_OWNER")
+                await self.four.join_chat("TGSTYLENAME")
+                await self.four.join_chat("BOTXPRO1")
             except:
                 pass
             assistants.append(4)
@@ -170,8 +170,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("BWF_MUSIC1")
-                await self.five.join_chat("MUSICBOT_OWNER")
+                await self.five.join_chat("TGSTYLENAME")
+                await self.five.join_chat("BOTXPRO1")
             except:
                 pass
             assistants.append(5)
