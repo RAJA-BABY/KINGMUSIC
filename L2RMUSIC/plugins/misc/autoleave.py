55f33b50e6b03a5967ec78331fe2382e3539c8ff
ASHISHTIME <155893081+BWFTIME@users.noreply.github.com>
2025-01-03 14:41:39 +0530

    ❖ ➥ 𝐿2𝙍 𝗠𝗨𝗦𝗜𝗖™🇮🇳

--- L2RMUSIC/plugins/misc/autoleave.py
+++ L2RMUSIC/plugins/misc/autoleave.py
@@ -9,7 +9,39 @@ from L2RMUSIC.core.call import Ashish, autoend
 from L2RMUSIC.utils.database import get_client, is_active_chat, is_autoend
 
 
+async def auto_leave():
+    if config.AUTO_LEAVING_ASSISTANT:
+        while not await asyncio.sleep(900):
+            from L2RMUSIC.core.userbot import assistants
 
+            for num in assistants:
+                client = await get_client(num)
+                left = 0
+                try:
+                    async for i in client.get_dialogs():
+                        if i.chat.type in [
+                            ChatType.SUPERGROUP,
+                            ChatType.GROUP,
+                            ChatType.CHANNEL,
+                        ]:
+                            if (
+                                i.chat.id != config.LOGGER_ID
+                                and i.chat.id != -1001465277194
+                                and i.chat.id != -1002120144597
+                            ):
+                                if left == 20:
+                                    continue
+                                if not await is_active_chat(i.chat.id):
+                                    try:
+                                        await client.leave_chat(i.chat.id)
+                                        left += 1
+                                    except:
+                                        continue
+                except:
+                    pass
+
+
+asyncio.create_task(auto_leave())
 
 
 async def auto_end():
