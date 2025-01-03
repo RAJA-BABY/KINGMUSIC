be96aec1edbd48c2375056abeb2df2924ce5bd0e
ʀᴀᴊᴀ ʙᴀʙᴜ <anilkumarmishraji80@gmail.com>
2025-01-03 19:57:22 +0530

    Update autoleave.py

--- L2RMUSIC/plugins/misc/autoleave.py
+++ L2RMUSIC/plugins/misc/autoleave.py
@@ -11,7 +11,7 @@ from L2RMUSIC.utils.database import get_client, is_active_chat, is_autoend
 
 async def auto_leave():    ### ❖ ➥ 𝗕𝐖𝗙 𝗠𝗨𝗦𝗜𝗖™🇮🇳
     if config.AUTO_LEAVING_ASSISTANT:
-        while not await asyncio.sleep(500):
+        while not await asyncio.sleep(900):
             from L2RMUSIC.core.userbot import assistants
 
             for num in assistants:
